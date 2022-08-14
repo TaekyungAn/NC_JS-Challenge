19c4514ac58b4e11600a5f8596c9147ebc28e3a8
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-08-14 13:12:31 +0900

    Adding date

--- 22.08.12/src/time.js
+++ 22.08.12/src/time.js
@@ -1,12 +1,26 @@
 const clock = document.querySelector("#clock");
-const day = document.querySelector("#day");
+const today = document.querySelector("#today");
+const date = new Date();
 
+// 실시간 시계 나타내기
 function realTimeClock() {
-  const date = new Date();
   const hr = String(date.getHours()).padStart(2, "0");
   const min = String(date.getMinutes()).padStart(2, "0");
   const sec = String(date.getSeconds()).padStart(2, "0");
   clock.innerHTML = `${hr}:${min}:${sec}`;
+  console.log(`clock wokring`);
 }
+
+// 년.월.일 요일 가져오기
+function todayDate() {
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
+  const yr = String(date.getFullYear());
+  const mon = String(date.getMonth() + 1).padStart(2, "0");
+  const dt = String(date.getDate()).padStart(2, "0");
+  const dayofWeek = week[date.getDay()];
+  today.innerHTML = `${yr}.${mon}.${dt} ${dayofWeek}`;
+}
+
+todayDate();
 realTimeClock();
 setInterval(realTimeClock, 1000);

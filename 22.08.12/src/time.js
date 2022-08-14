6a6972dd984f62c2679b5d4740e7f5d02154880d
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-08-14 16:05:51 +0900

    실시간 시간변화 디버깅

--- 22.08.12/src/time.js
+++ 22.08.12/src/time.js
@@ -1,18 +1,18 @@
-const clock = document.querySelector("#clock");
-const today = document.querySelector("#today");
-const date = new Date();
+const clock = document.querySelector("h1#clock");
+// const today = document.querySelector("#today");
 
 // 실시간 시계 나타내기
 function realTimeClock() {
+  const date = new Date(); // setInterval이 실행 될 때마다 계속 불러와야 함
   const hr = String(date.getHours()).padStart(2, "0");
   const min = String(date.getMinutes()).padStart(2, "0");
   const sec = String(date.getSeconds()).padStart(2, "0");
   clock.innerHTML = `${hr}:${min}:${sec}`;
-  console.log(`clock wokring`);
 }
 
 // 년.월.일 요일 가져오기
 function todayDate() {
+  const date = new Date();
   const week = ["일", "월", "화", "수", "목", "금", "토"];
   const yr = String(date.getFullYear());
   const mon = String(date.getMonth() + 1).padStart(2, "0");
